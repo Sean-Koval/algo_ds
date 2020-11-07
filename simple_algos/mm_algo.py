5551a4e22bc7742cf216a47527467cd392251b14
Sean Koval <sean@capitalprawn.com>
2020-11-07 15:23:02 -0500

    side bet

---
+++ simple_algos/mm_algo.py
@@ -0,0 +1,2 @@
+# this sis a an algo folr determing the side of the market maker bit using a primary algo to determine side
+# we weill then iuse a secoindarty aklfdo to determine the bet size (0,1)
