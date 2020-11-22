dea4198414b530a3df36893730f2258ac80927bd
Sean Koval <sean@capitalprawn.com>
2020-11-21 23:34:31 -0500

    update

--- work_algos/linked_list.py
+++ work_algos/linked_list.py
@@ -1,12 +1,12 @@
 class Node:
-    
+
     def __init__(self, data=None):
         self.data = data
         # unless the value is changed to next, this will be the endpoint
         self.next = None
     def __str__(self):
         return str(data)
-    
+
 # create the nodes for the linked list
 n1 = Node('eggs')
 n2 = Node('ham')
