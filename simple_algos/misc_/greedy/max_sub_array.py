6c7d296ae959713c63660527551e7721c8e2d71f
Sean Koval <sean@capitalprawn.com>
2020-12-14 13:11:39 -0500

    added sec solution

--- simple_algos/misc_/greedy/max_sub_array.py
+++ simple_algos/misc_/greedy/max_sub_array.py
@@ -4,7 +4,7 @@
 # Space: 14.1 mb (87%)
 
 
-
+# SOLUTION 1: Greedy
 class Solution(object):
     def maxSubArray(self, nums):
         """
@@ -25,3 +25,35 @@ class Solution(object):
 
 
         return max_sum
+
+# SOLUTION 2: Merge Sort
+    class Solution:
+        def cross_sum(self, nums, left, right, p): 
+            if left == right:
+                return nums[left]
+
+            left_subsum = float('-inf')
+            curr_sum = 0
+            for i in range(p, left - 1, -1):
+                curr_sum += nums[i]
+                left_subsum = max(left_subsum, curr_sum)
+
+            right_subsum = float('-inf')
+            curr_sum = 0
+            for i in range(p + 1, right + 1):
+                curr_sum += nums[i]
+                right_subsum = max(right_subsum, curr_sum)
+
+            return left_subsum + right_subsum   
+
+    def helper(self, nums, left, right): 
+        if left == right:
+            return nums[left]
+
+        p = (left + right) // 2
+
+        left_sum = self.helper(nums, left, p)
+        right_sum = self.helper(nums, p + 1, right)
+        cross_sum = self.cross_sum(nums, left, right, p)
+
+        return max(left_sum, right_sum, cross_sum)
